2c8186cb2988502a7fbf3f47176c94e557c54db1
andrew6000 <ag91375@gmail.com>
2017-07-17 07:20:53 -0400

    update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 
 module.exports = {
- consumer_key:         'j1BKeb09IMK1isyzr5LsDlmB4',
-  consumer_secret:      'ZS5O5UKDSNHzOk2UxsIB3Bi8gJ0L7cr9lE6WKzbL5pXwjp52i4',
-  access_token:         '255718856-YbNqgMrdtlUrf83zg3bbqPEOagXshNZgdnVN3YUu',
-  access_token_secret:  'nmYGRx6rf7CKEqko8yLfIVElHrEpGZ2q1zwUa0m51rXKG'
+ consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
